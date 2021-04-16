a7fb86efff753038672fe26767cafc783c63054e
Tobias Weber <tobias.weber@tu-dortmund.de>
2021-04-16 12:36:00 +0200

    reworks the logging of reward and episode length for ppo2

--- nm_baselines/baselines/baselines/ppo2/ppo2.py
+++ nm_baselines/baselines/baselines/ppo2/ppo2.py
@@ -160,8 +160,9 @@ def learn(*, network, env, total_timesteps, eval_env = None, seed=None, nsteps=1
         obs, returns, masks, actions, values, neglogpacs, states, epinfos = runner.run() #pylint: disable=E0632
 
         if nm_customization_args['log_model_parameters'] and epinfos:
-            ex.log_scalar('ep_reward', epinfos[0]['r'], update)
-            ex.log_scalar('ep_length', epinfos[0]['l'], update)
+            for entry in epinfos:
+                ex.log_scalar('ep_reward', entry['r'])
+                ex.log_scalar('ep_length', entry['l'])
 
         if eval_env is not None:
             eval_obs, eval_returns, eval_masks, eval_actions, eval_values, eval_neglogpacs, eval_states, eval_epinfos = eval_runner.run() #pylint: disable=E0632
